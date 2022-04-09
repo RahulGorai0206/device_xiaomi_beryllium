dc77153afc2ed669da6b24b57906e46b767199d3
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-04-09 13:08:34 +0530

    beryllium: spark-ify
    * lets goo sparks
    
    Signed-off-by: RahulGorai0206 <rahulgorai0206@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/spark_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    spark_beryllium-user \
+    spark_beryllium-userdebug \
+    spark_beryllium-eng
