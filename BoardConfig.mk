d81f635f3293a4a4ec1139f6d5dac78ece468163
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-05-22 11:28:33 +0530

    beryllium: Set tap to wake node path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,9 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67092480
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
