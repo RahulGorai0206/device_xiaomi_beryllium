c60f7adc9b3a395f3f9aa3834b3fabb308c75460
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-08-06 21:38:05 +0530

    beryllium: Bringup Project Elixir

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,15 +6,19 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
-
-# Inherit some common stuff.
+# Inherit some common Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := false
+IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
