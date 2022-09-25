bfc58358176c682c6731b38ecbd78bda7b229af6
Aman Singh <amankumarsingh1412@gmail.com>
2022-09-25 11:08:38 +0530

    beryllium: build vendor.qti.hardware.fm@1.0 from source

--- device.mk
+++ device.mk
@@ -17,7 +17,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     FM2 \
     libqcomfm_jni \
-    qcom.fmradio
+    qcom.fmradio \
+    vendor.qti.hardware.fm@1.0.vendor
+
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
